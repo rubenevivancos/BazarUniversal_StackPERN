b04849908eb832c96220c07a67a9f4a67fb79eeb
rubenevivancos <rubenevivancos@hotmail.com>
2024-03-22 18:44:15 -0500

    Configurando index

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
 
 
-
-server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+conn.sync().then(() => {
+    server.listen(3001, () => {
+      console.log('%s listening at 3001'); // eslint-disable-line no-console
+    });
 });
--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const {
 } = process.env;
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/Proyectos`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   define: {
