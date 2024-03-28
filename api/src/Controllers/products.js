5fedbcb4f29eb606f588d02644c478be8b280e27
rubenevivancos <rubenevivancos@hotmail.com>
2024-03-28 01:40:12 -0500

    Se cambio el store procedure por una function

--- api/src/Controllers/products.js
+++ api/src/Controllers/products.js
@@ -1,7 +1,7 @@
 const { conn } = require('../db.js'); // Importa tu instancia de Sequelize
 
 
-function productSearch(req, res){
+async function productSearch(req, res){
     console.log("[ productSearch ] INICIO");
     let { search } = req.query;
     console.log("[ productSearch ] search --> " + search);
@@ -10,38 +10,45 @@ function productSearch(req, res){
         search = search.toLowerCase();
         console.log("[ productSearch ] El producto a buscar es: " + search);
 
-        //Obteniendo el listado de la busqueda usando el store procedure
-        const result = getListProducts(search);
+        try{
+            //Obteniendo el listado de la busqueda usando la FUNCTION
+            const result = await getListProducts(search);
 
-        /*
-        //Obteniendo la cantidad de productos por categoria del listado de la busqueda: INICIO
-        const categories = result.map((object) => object.category);
-        const uniqueCategories = new Set(categories);
+            /*
+            //Obteniendo la cantidad de productos por categoria del listado de la busqueda: INICIO
+            const categories = result.map((object) => object.category);
+            const uniqueCategories = new Set(categories);
 
-        const categoriesWithCount = [];
+            const categoriesWithCount = [];
 
-        uniqueCategories.forEach((category) => {
-            const objectsByCategory = result.filter((object) => object.category === category);
-            categoriesWithCount.push({ category: category, count: objectsByCategory.length });
-        });
-        //Obteniendo la cantidad de productos por categoria del listado de la busqueda:FIN
+            uniqueCategories.forEach((category) => {
+                const objectsByCategory = result.filter((object) => object.category === category);
+                categoriesWithCount.push({ category: category, count: objectsByCategory.length });
+            });
+            //Obteniendo la cantidad de productos por categoria del listado de la busqueda:FIN
 
-        for(let i=0; i<categoriesWithCount.length; i++){
-            console.log("category -->" + categoriesWithCount[i].category);
-            console.log("count -->" + categoriesWithCount[i].count);
+            for(let i=0; i<categoriesWithCount.length; i++){
+                console.log("category -->" + categoriesWithCount[i].category);
+                console.log("count -->" + categoriesWithCount[i].count);
+            }
+            */
+            const categoriesWithCount = [];
+            let resul = {listProducts: result, categoriesWithCount: categoriesWithCount};
+
+            if(result.length){
+                console.log("[ productSearch ] Se encontraron " + result.length + " resultados");
+                console.log("[ productSearch ] product_title: " + result[0].product_title);
+                console.log("[ productSearch ] category_name: " + result[0].category_name);
+                return res.status(200).json(resul);
+            }
+            console.log("[ productSearch ] No hay resultados");
+            return res.status(422).json({message: "No hay resultados"}); 
+
+        } catch (error) {
+            console.log("[ productSearch ] Ocurrio una excepcion: " + error.message);
+            return res.status(404).send(error.message);
         }
-        */
-        const categoriesWithCount = [];
-        let resul = {listProducts: result, categoriesWithCount: categoriesWithCount};
 
-        if(result.length){
-            console.log("[ productSearch ] Se encontraron " + result.length + " resultados");
-            console.log("[ productSearch ] product_title: " + result[0].product_title);
-            console.log("[ productSearch ] category_name: " + result[0].category_name);
-            return res.status(200).json(resul);
-        }
-        console.log("[ productSearch ] No hay resultados");
-        return res.status(422).json({message: "No hay resultados"}); 
     }
     
     return res.status(200).json([]);
@@ -71,14 +78,14 @@ function getDetail(req, res){
 
 async function getListProducts(search) {
     try {
-        const results = await conn.query('CALL get_product_category_names(:search)', {
+        const results = await conn.query('SELECT * FROM "BazarUniversal".get_product_category_names(:search)', {
             replacements: { search: search },
             type: conn.QueryTypes.SELECT
         });
     
         return results;
     } catch (error) {
-        console.error('Error al llamar al procedimiento almacenado:', error);
+        console.error('Error al llamar a la funcion:', error);
         throw error;
     }
 }
