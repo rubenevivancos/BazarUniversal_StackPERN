b82490da335615df1d28f28b0ae2d5436a429272
rubenevivancos <rubenevivancos@hotmail.com>
2024-05-20 17:38:11 -0500

    Correccion en el tamaño del fondo

--- client/src/Components/SearchResults/searchResults.js
+++ client/src/Components/SearchResults/searchResults.js
@@ -52,8 +52,8 @@ export default function SearchResults() {
 
     if(listProducts.length){
         return(
-            <>
-                <Container fluid className="py-4" style={{ maxHeight: '90vh', maxWidth: '50vh', backgroundColor: '#fdfd96' }}>
+            <div className="d-flex justify-content-center align-items-start" style={{ height: '100vh', backgroundColor: '#fdfd96' }}>
+                <Container fluid className="py-4" style={{ maxHeight: '90vh', maxWidth: '50vh' }}>
                     <Row className="mb-2">
                         <GoBack/>
                     </Row>    
@@ -99,7 +99,7 @@ export default function SearchResults() {
                         </Col>
                     </Row>
                 </Container>
-            </>
+            </div>
         )
     }
 }
