b592c87348ce37c47b83ce24787021dae396883d
rubenevivancos <rubenevivancos@hotmail.com>
2025-01-07 12:34:30 -0500

    style: arranging the search result in 3 columns

--- client/src/Components/SearchResults/searchResults.js
+++ client/src/Components/SearchResults/searchResults.js
@@ -58,44 +58,50 @@ export default function SearchResults() {
                     <Row className="mb-2">
                         <MainHeader/>
                     </Row>   
-                    <Row className="mb-2">
-                        <GoBack/>
-                    </Row>
                     <Row>
-                        <Col>
-                            <Row className="mb-4 justify-content-left">
-                                <Col xs={12} md={10} className="text-left">
-                                    <div>Resultados de la busqueda de " {productToSearch} ": {listProducts.length}</div>
-                                </Col>
+                        <Container className="w-75">
+                            <Row className="mb-2">
+                                <GoBack/>
                             </Row>
-                            <Row className="mb-4">
-                                <Col className="d-flex flex-wrap justify-content-start">
-                                    {categoriesWithCount.map((cat, index) => (                            
-                                        <span 
-                                            key={index} 
-                                            className="me-2 p-1 mb-1 rounded small fw-bold" 
-                                            style={{ backgroundColor: generateColor(cat.category) }}
-                                        >
-                                            {cat.category} - {cat.count}
-                                        </span>
-                                    ))}
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col>
                             <Row>
-                                <Col className="text-left">
-                                    <div>
-                                        { listProducts.map( product => <Link to={"/items/"+product.p_id} key={product.p_id} style={{ textDecoration: 'none' }}><Product product={product} /></Link>) }
-                                    </div>
+                                <Col md={3}>
+                                    <Row className="mb-4 justify-content-left">
+                                        <Col xs={12} md={10} className="text-left">
+                                            <div><b><h1>{productToSearch}</h1></b><br/>{listProducts.length} resultados</div>
+                                        </Col>
+                                    </Row>
+                                    <Row className="mb-4">
+                                        <Col className="d-flex flex-wrap justify-content-start">
+                                            {categoriesWithCount.map((cat, index) => (                            
+                                                <span 
+                                                    key={index} 
+                                                    className="me-2 p-1 mb-1 rounded small fw-bold" 
+                                                    style={{ backgroundColor: generateColor(cat.category) }}
+                                                >
+                                                    {cat.category} - {cat.count}
+                                                </span>
+                                            ))}
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                <Col md={6}>
+                                    <Row>
+                                        <Col className="text-left">
+                                            <div>
+                                                { listProducts.map( product => <Link to={"/items/"+product.p_id} key={product.p_id} style={{ textDecoration: 'none' }}><Product product={product} /></Link>) }
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                <Col md={3}>
+                                    <Row>
+                                        <Col className="text-end">
+                                            Ordenar por
+                                        </Col>
+                                    </Row>
                                 </Col>
                             </Row>
-                        </Col>
-                        <Col>
-                            <Row>
-                                Ordenar por
-                            </Row>
-                        </Col>
+                        </Container>
                     </Row>
                 </Container>
             </div>
