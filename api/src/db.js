4387bf80fe435d8ef9d91a5c67a5df084eed82ba
rubenevivancos <rubenevivancos@hotmail.com>
2025-02-03 16:25:31 -0500

    feat: update database connection configuration to cloud (Aiven)

--- api/src/db.js
+++ api/src/db.js
@@ -1,14 +1,20 @@
 import { config } from 'dotenv';
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 
 config();  // Carga las variables de entorno desde el archivo .env
 
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, CA_CERTIFICATE } = process.env;
 
+pg.defaults.ssl = {
+  require: true,
+  rejectUnauthorized: false, //En true verifica que el certificado sea valido, en produccion debe ser true
+  ca: CA_CERTIFICATE
+};
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  dialect: 'postgres',
+  dialectModule: pg, // Utiliza el cliente de PostgreSQL que es pg
   logging: false, // set to console.log to see the raw SQL queries
   native: false // lets Sequelize know we can use pg-native for ~30% more speed
   //define: {
