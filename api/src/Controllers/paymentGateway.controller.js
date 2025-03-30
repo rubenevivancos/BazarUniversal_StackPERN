62793056fe5ad2cfb3d303b6d45b27bcef9fa946
rubenevivancos <rubenevivancos@hotmail.com>
2025-03-30 17:03:11 -0500

    feat: start Stripe integration in the backend

---
+++ api/src/Controllers/paymentGateway.controller.js
@@ -0,0 +1,43 @@
+import paymentGatewayService from "../Services/paymentGateway.service.js";
+
+
+const paymentGatewayController = {
+
+    async createSession(req, res) {
+        const { name, currency, unit_amount, quantity, mode, success_url, cancel_url } = req.body;
+
+        try {
+
+            const session = await paymentGatewayService.createSession({ name, currency, unit_amount, quantity, mode, success_url, cancel_url });
+            res.status(200).json({ message: "Stripe exitoso", session: session });
+
+        } catch (error) {
+            res.status(error.statusCode || 500).json({ message: error.message });
+        }
+    },
+
+    async getCheckoutSession(req, res) {
+        const { sessionID } = req.query;
+
+        try {
+
+            const session = await paymentGatewayService.getCheckoutSession(sessionID);
+            res.status(200).json({ message: "Session de Stripe exitoso", session: session });
+
+        } catch (error) {
+            res.status(error.statusCode || 500).json({ message: error.message });
+        }
+    },
+
+    async cancel(req, res) {
+        try {
+
+            res.redirect('/');
+            
+        } catch (error) {
+            res.status(error.statusCode || 500).json({ message: error.message });
+        }
+    }
+};
+
+export default paymentGatewayController;
