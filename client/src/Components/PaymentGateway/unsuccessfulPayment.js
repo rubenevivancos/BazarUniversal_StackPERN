a2ae6b221b19140aaa9f63965a27721088e44101
rubenevivancos <rubenevivancos@hotmail.com>
2025-03-29 23:27:30 -0500

    feat: add button to return to home on successful payment and unsuccessful payment pages

--- client/src/Components/PaymentGateway/unsuccessfulPayment.js
+++ client/src/Components/PaymentGateway/unsuccessfulPayment.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Container, Row } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
 
 
 import MainHeader from '../Header/mainHeader';
-import GoBack from '../GoBack/goBack';
 
 
 export default function UnsuccessfulPayment() {
 
+    const navigate = useNavigate();
     const storedUser = localStorage.getItem("user");
     const user = useSelector((state) => state.userReducer.user) || (storedUser ? JSON.parse(storedUser) : null);
-    console.log("user --> " + user);
+
+    const goToHome = (e) => {
+        e.preventDefault();
+        navigate("/");
+    };
 
     return(
         <Container fluid style={{ backgroundColor: '#fdfd96', minHeight: '100vh' }}>
@@ -21,10 +26,13 @@ export default function UnsuccessfulPayment() {
             <Row className="mb-2">
                 <Container className="w-75">
                     <Row className="mb-4">
-                        <GoBack/>
+                        <br></br>
                     </Row>
                     <Row>
                         LO SENTIMOS {user.name}, TU PAGO NO SE PUDO REALIZAR
+                    </Row>
+                    <Row className="justify-content-end">
+                        <Button variant="primary" className="w-auto" onClick={goToHome}>Ir al inicio</Button>
                     </Row>
                 </Container>
             </Row>
